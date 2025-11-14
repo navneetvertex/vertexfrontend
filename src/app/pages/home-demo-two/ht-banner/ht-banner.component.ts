c7af2a4c36b5cfea4b3f0e85012e727f1c91bcd1
root <mukesh123ba@gmail.com>
2025-11-13 22:05:41 -0500

    Latest 13 Nov

--- src/app/pages/home-demo-two/ht-banner/ht-banner.component.ts
+++ src/app/pages/home-demo-two/ht-banner/ht-banner.component.ts
@@ -32,69 +32,38 @@ export class HtBannerComponent implements AfterViewInit {
   // activeSlide = 0;
   // intervalId: any;
   currentIndex = 0;
-  ngAfterViewInit() {
-    new Swiper('.mySwiper', {
-      loop: true,
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false
-      },
-      speed: 1000,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      }
-    });
- // Define your custom function here
 
-      // items: [
-      //   { image: 'images/FD.jpg', text: 'Item 1' },
-      //   { image: 'images/Franchise2.jpg', text: 'Item 2' },
-      //   { image: 'images/FD.jpg', text: 'Item 3' },
-      //   { image: 'images/hand-shake.jpg', text: 'Item 4' },
-      //   { image: 'images/img5.jpg', text: 'Item 5' },
-      //   { image: 'images/hand-shake.jpg', text: 'Item 6' },
-      // ]
-  
-  // ngAfterViewInit() {
-    // this.controls.toArray().forEach((control, idx) => {
-    //   control.nativeElement.addEventListener('click', () => {
-    //     this.activeSlide = idx;
-    //     this.changeSlides();
-    //     clearInterval(this.intervalId);
-    //     this.intervalId = setInterval(() => this.changeSlides(), 4000);
-    //   });
-    // });
-  // }
-
-  // changeSlides() {
-  //   console.log('Slide changed to:', this.activeSlide);
-  //   // Add actual slide change logic here
-  // }
-  // ngOnInit() {
-  //   this.myCustomFunction("Hello Mukesh from TS!");
-  // }
-
-  // myCustomFunction(msg: string) {
-  //   console.log("TS Function:", msg);
-  // }
-  // ngOnInit(): void {
+//   ngAfterViewInit() {
+//     new Swiper('.mySwiper', {
+//       loop: true,
+//       autoplay: {
+//         delay: 3000,
+//         disableOnInteraction: false
+//       },
+//       speed: 1000,
+//       pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true
+//       },
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev'
+//       }
+//     });
  
-    //  myCustomFunction();
+// }
+// }
 
-    //   AOS.init({
-    //     duration: 1200,   // Animation duration
-    //     easing: 'ease-out-cubic', 
-    //     once: true,       // Run only once
-    //   });
-    // }
-  // }
+ngAfterViewInit(): void {
+  new Swiper('.mySwiper', {
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    speed: 1000,
+    effect: 'slide',
+  });
 }
 }
-
- 
 
